1ab2e0025fa4a177c51de7e0042357af3ae27f48
Rishav Rapta <rishavrapta@Rishavs-Air.lan>
2021-03-15 09:54:32 +0200

    contact-form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,11 +1,6 @@
 import React, { ChangeEvent, useEffect, useState, FormEvent } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  industryData,
-  countryData,
-  operatingGeography,
-} from "../../data/index";
 import { store } from "../../store/reducer";
 import Form from "./form";
 import {
